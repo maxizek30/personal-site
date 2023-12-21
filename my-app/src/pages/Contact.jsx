0283ee4a0778142b3547769adf4056589679c8f3
Max Lopez <familiacachorin@gmail.com>
2023-12-20 20:29:56 -0800

    arrange and add styling to social icons in Contact page

--- my-app/src/pages/Contact.jsx
+++ my-app/src/pages/Contact.jsx
@@ -1,12 +1,15 @@
 import LinkedInIcon from "../assets/linkedin-icon.svg"
 import GithubIcon from "../assets/github-icon.svg"
 import EmailIcon  from "../assets/email-icon.svg"
+import "../styles/Contact.css"
 function Contact() {
   return (
     <div className="SocialMedia-Icons">
+      <img src={EmailIcon} alt="LinkedIn Icon" />   
       <img src={LinkedInIcon} alt="LinkedIn Icon" />
       <img src={GithubIcon} alt="LinkedIn Icon" />
-      <img src={EmailIcon} alt="LinkedIn Icon" />      
+
+   
     </div>
   )
 }
