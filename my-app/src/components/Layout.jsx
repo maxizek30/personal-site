2bd183aa2115da6c6986cb7a12f9e917be485b21
Max Lopez <familiacachorin@gmail.com>
2023-12-28 16:41:58 -0800

    make project responsive

--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router-dom";
+import { useState } from 'react';
+import { NavLink, Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import DarkModeToggle from "./DarkModeToggle";
 import { useDarkMode } from "../context/DarkModeContext";
@@ -7,30 +8,44 @@ import "../styles/Layout.css"
 const Layout = ({ children }) => {
 
     const { darkMode } = useDarkMode(); // get the current dark mode state
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const layoutClass = darkMode ? 'layout dark-mode' : 'layout';
     const headerClass = `header ${darkMode ? 'dark-mode' : ''}`;
     const footerClass = `footer ${darkMode ? 'dark-mode' : ''}`;
+    const headerLinksClass = `header-links ${darkMode ? 'dark-mode' : ''} ${isMenuOpen ? 'active' : ''}`;
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    };
 
     return(
         <div className={layoutClass}>
             <header className={headerClass}>
-                <div className="header-title">Max Lopez</div>
-                <div className="header-links">
+                <div className="header-top">
+                    <div className="header-title">
+                        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>Max Lopez</Link>
+                    </div>
+                    <div className="menu-icon" onClick={toggleMenu}>☰</div>
+                </div>
+                <div className={headerLinksClass}>
                     <NavLink 
                     to="/"
+                    onClick={toggleMenu}
                     className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}
                     >
                         Home
                     </NavLink>
                     <NavLink 
                     to="/portfolio"
+                    onClick={toggleMenu}
                     className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}
                     >
                         Portfolio
                     </NavLink>
                     <NavLink 
                     to="/contact"
+                    onClick={toggleMenu}
                     className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}
                     >
                         Contact
--- my-app/src/pages/Contact.jsx
+++ my-app/src/pages/Contact.jsx
@@ -5,11 +5,15 @@ import "../styles/Contact.css"
 function Contact() {
   return (
     <div className="SocialMedia-Icons">
-      <img src={EmailIcon} alt="LinkedIn Icon" />   
-      <img src={LinkedInIcon} alt="LinkedIn Icon" />
-      <img src={GithubIcon} alt="LinkedIn Icon" />
-
-   
+      <a href="mailto:familiacachorin@gmail.com">
+        <img src={EmailIcon} alt="Email Icon" />   
+      </a>
+      <a href="https://www.linkedin.com/in/max-lopez-2b3b17242">
+        <img src={LinkedInIcon} alt="LinkedIn Icon" />
+      </a>
+      <a href="https://github.com/maxizek30">
+        <img src={GithubIcon} alt="Github Icon" />
+      </a>
     </div>
   )
 }
