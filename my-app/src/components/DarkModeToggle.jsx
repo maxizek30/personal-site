283b1945c7195928ad23fb077e77b091bd33e86d
Max Lopez <familiacachorin@gmail.com>
2024-01-22 17:10:31 -0800

    add text to darkMode button

--- my-app/src/components/DarkModeToggle.jsx
+++ my-app/src/components/DarkModeToggle.jsx
@@ -1,19 +1,35 @@
 import { useDarkMode } from "../context/DarkModeContext";
 import "../styles/DarkModeToggle.css"
+import { useEffect, useState } from "react";
 
 const DarkModeToggle = () => {
     const { darkMode, toggleDarkMode } = useDarkMode();
+    const [buttonText, setbuttonText] = useState(darkMode ? "Dark Mode" : "Light Mode");
+
+    useEffect(() => {
+        if(darkMode) {
+            setbuttonText("Dark Mode");
+        } else {
+            setbuttonText("Light Mode");
+        }
+    }, [darkMode]);
+    
 
     return (
-        <label className="switch">
-            <input 
-                type="checkbox" 
-                checked={darkMode} 
-                onChange={toggleDarkMode} 
-                readOnly
-            />
-            <div className="slider round"></div>
-        </label>
+        <>
+            <div className="buttonContainer">
+                {buttonText}
+                <label className="switch">
+                    <input 
+                        type="checkbox" 
+                        checked={darkMode} 
+                        onChange={toggleDarkMode} 
+                        readOnly
+                    />
+                    <div className="slider round"></div>
+                </label>
+            </div>
+        </>
     );
 };
 
