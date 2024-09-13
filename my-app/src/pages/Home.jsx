874c3596e711189e049d6777868d4d537d1128a6
Max Lopez <familiacachorin@gmail.com>
2024-09-12 21:52:42 -0700

    Update home description

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,21 +1,25 @@
-import "../styles/Home.css"
-import { useDarkMode } from "../context/DarkModeContext"
+import "../styles/Home.css";
+import { useDarkMode } from "../context/DarkModeContext";
 
 function Home() {
-
   const { darkMode } = useDarkMode();
 
-  const welcomedescriptionClass = `Welcome-Description ${darkMode ? 'dark-mode' : ''}`;
+  const welcomedescriptionClass = `Welcome-Description ${
+    darkMode ? "dark-mode" : ""
+  }`;
 
   return (
-    <> 
+    <>
       <div className="Welcome-Title">Welcome.</div>
       <div className={welcomedescriptionClass}>
         <div className="Welcome-Vertical-Line" />
-        My name is Max Lopez, a sophomore at Pacific Lutheran University majoring in Computer Science. Eager to dive deep into the world of technology, I'm all about learning and applying foundational concepts, exploring vast possibilities, and contributing to innovative solutions.
+        My name is Max Lopez, a junior at Pacific Lutheran University majoring
+        in Computer Science. Eager to dive deep into the world of technology,
+        I'm all about learning and applying foundational concepts, exploring
+        vast possibilities, and contributing to innovative solutions.
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
